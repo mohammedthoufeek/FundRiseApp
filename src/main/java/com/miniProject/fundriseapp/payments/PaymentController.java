8b69f4cb3c15433f252518f0a962124505c321d3
Atharsh G <atharsh.gkrishnan@gmail.com>
2024-02-29 11:53:26 +0530

    final commit

--- src/main/java/com/miniProject/fundriseapp/payments/PaymentController.java
+++ src/main/java/com/miniProject/fundriseapp/payments/PaymentController.java
@@ -1,4 +1,33 @@
 package com.miniProject.fundriseapp.payments;
 
+import com.miniProject.fundriseapp.account.AccountDto;
+import com.miniProject.fundriseapp.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("payment")
+    public Payments addPayment(@RequestBody PaymentDto paymentDto) throws PaymentsException {
+        return this.paymentService.addPayments(paymentDto);
+    }
+
+    @GetMapping("payment")
+    public List<Payments> getAllPayments(){
+        return this.paymentService.getAllPayments();
+    }
+
+    @GetMapping("payment/{id}")
+    public Payments getPaymentById(@PathVariable Integer id){
+        return this.paymentService.getPaymentById(id);
+    }
+
+
+
 }
